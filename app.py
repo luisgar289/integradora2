981994e22419dab029e0e165b41efdb49b602f2f
Luis Garcia <77448672+luisgar289@users.noreply.github.com>
2022-03-21 22:36:47 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import web
+
+urls = (
+    '/', 'Index',
+    '/talleres', 'Talleres',
+    '/referencias', 'Referencias',
+    '/contacto','Contacto',
+	'/amistad', 'Amistad',
+    '/ansiedad', 'Ansiedad',
+    '/depresion', 'Depresion',
+    '/hobby', 'Hobby',
+    '/terapia', 'Terapia',
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+
+class Index:
+	def GET(self):
+		return render.index()
+
+class Talleres:
+	def GET(self):
+		return render.talleres()
+
+class Referencias:
+	def GET(self):
+		return render.referencias()
+
+class Contacto:
+	def GET(self):
+		return render.contacto()
+
+class Ansiedad:
+	def GET(self):
+		return render.ansiedad()
+
+class Amistad:
+	def GET(self):
+		return render.amistad()
+
+class Depresion:
+	def GET(self):
+		return render.depresion()
+
+class Hobby:
+	def GET(self):
+		return render.hobby()
+
+class Terapia:
+	def GET(self):
+		return render.terapia()
+
+
+if __name__ == "__main__": 
+    web.config.debug = False
+    app.run()
